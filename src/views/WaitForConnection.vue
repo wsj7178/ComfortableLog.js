add5da5b7de2fa77e6c9ee3721a6492eb34457e9
wsj7178 <wsj7178@gmail.com>
2020-07-20 23:15:53 +0900

    add init page

---
+++ src/views/WaitForConnection.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="">
+
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class WaitForConnection extends Vue {
+
+}
+</script>
+
+<style>
+
+</style>
